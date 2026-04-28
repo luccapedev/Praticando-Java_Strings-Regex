17d8ac47725baf59a127ced61afc793ac1ec04c5
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-04-28 09:21:34 -0300

    atividade 1 concluída

--- src/service/AtividadeUm.java
+++ src/service/AtividadeUm.java
@@ -2,4 +2,13 @@ package service;
 import java.util.Scanner;
 
 public class AtividadeUm {
+    public AtividadeUm() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome: ");
+        String nome = input.nextLine();
+
+        String nomeFormatado = nome.trim();
+        System.out.println("Nome formatado: " + nomeFormatado);
+        input.close();
+    }
 }
