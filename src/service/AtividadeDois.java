dc0528d08714e2cd3e166b7473d766337de65358
Lucca Pedroza <luccaesu2003@hotmail.com>
2026-04-28 09:24:35 -0300

    atividade 2 concluída

--- src/service/AtividadeDois.java
+++ src/service/AtividadeDois.java
@@ -2,4 +2,16 @@ package service;
 import java.util.Scanner;
 
 public class AtividadeDois {
+    public AtividadeDois() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o texto: ");
+        String texto = input.nextLine();
+
+        String textoMaisuculo = texto.toUpperCase();
+        System.out.println("Texto em maiúsculas: " + textoMaisuculo);
+
+        String textoMinusculo  = texto.toLowerCase();
+        System.out.println("Texto em minúsculas: " + textoMinusculo);
+        input.close();
+    }
 }
